6d2421f4e942c7d66f9477c56dc615ee05ec66ac
Tan Su <Tan@Ridecell.com>
2019-03-07 15:47:54 -0800

    added radio_station entity

---
+++ entities/radio_station.py
@@ -0,0 +1,39 @@
+class RadioStation:
+
+    def __init__(self, radio_station_id, frequency, genre, name):
+        self.radio_station_id = radio_station_id
+        self.frequency = frequency
+        self.genre = genre
+        self.name = name
+
+    @property
+    def radio_station_id(self):
+        return self.radio_station_id
+
+    @radio_station_id.setter
+    def id(self, value):
+        self.radio_station_id = value
+
+    @property
+    def frequency(self):
+        return self.frequency
+
+    @frequency.setter
+    def id(self, value):
+        self.frequency = value
+
+    @property
+    def genre(self):
+        return self.genre
+
+    @genre.setter
+    def genre(self, value):
+        self.genre = value
+
+    @property
+    def name(self):
+        return self.name
+
+    @id.setter
+    def name(self, value):
+        self.name = value
